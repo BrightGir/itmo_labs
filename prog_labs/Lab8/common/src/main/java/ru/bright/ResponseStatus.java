df24d2406370e93ee88e5e1bdffe9ab659d235f0
BrightGirl <fonareva2006@mail.ru>
2025-10-26 22:20:58 +0300

    chore: move prog-lab8 to another repository

--- prog_labs/Lab8/common/src/main/java/ru/bright/ResponseStatus.java
+++
@@ -1,7 +0,0 @@
-package ru.bright;
-
-import java.io.Serializable;
-
-public enum ResponseStatus implements Serializable {
-    OK, ERROR, DENIED, AUTH_FAILED, AUTH_PASSED, LOGIN_BUSY, REGISTER_SUCCESSFUL, UPDATE
-}
